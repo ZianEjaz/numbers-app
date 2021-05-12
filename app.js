0f7d0e427b90ce2a2517cb3db05b4d709fdf2534
Zian Ejaz <zianejaz@gmail.com>
2021-05-12 17:48:32 +0500

    added new module to sort contacts countrywise, working on sort contacts countrywise file

--- app.js
+++ app.js
@@ -16,32 +16,40 @@ xlsxFile('./dummy.xlsx').then((rows) => {
 //get requests
 
   app.get('/', (req, res) => {
-    res.render("home.ejs");
+    res.render("components/home.ejs");
   });
   app.get('/addDataBefore',(req,res)=>{
-    res.render("text before.ejs");
+    res.render("components/text before.ejs");
   })
   app.get('/addDataAfter',(req,res)=>{
-    res.render("text after.ejs");
+    res.render("components/text after.ejs");
   })
   app.get('/sortContacts',(req,res)=>{
-    res.render("sort contacts.ejs");
+    res.render("components/sort contacts.ejs");
+  })
+  app.get('/sortCountry',(req,res)=>{
+    res.render("components/sort country wise.ejs", {numbers : rows});
   })
 
 //post requests
 
 app.post('/addDataBefore', (req, res) =>{
   const input = req.body.dataBefore;
-  res.render("text before return.ejs", {numbers : rows, dataBefore : input });
+  res.render("post/text before return.ejs", {numbers : rows, dataBefore : input });
 })
 app.post('/addDataAfter', (req, res) =>{
   const input = req.body.dataAfter;
-  res.render("text after return.ejs", {numbers : rows, dataAfter : input });
+  res.render("post/text after return.ejs", {numbers : rows, dataAfter : input });
 })
 app.post('/sortContacts', (req, res) =>{
   const totalDigits = req.body.totalDigits;
   const firstDigit = req.body.firstDigit;
-  res.render("sort contacts return.ejs ", {numbers : rows, totalDigits : totalDigits, firstDigit : firstDigit });
+  res.render("post/sort contacts return.ejs ", {numbers : rows, totalDigits : totalDigits, firstDigit : firstDigit });
+})
+app.post('/sortCountry', (req, res) =>{
+  const totalDigits = req.body.totalDigits;
+  const firstDigit = req.body.firstDigit;
+  res.render("post/sort country wise return.ejs ", {numbers : rows, totalDigits : totalDigits, firstDigit : firstDigit });
 })
 
 
