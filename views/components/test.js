a4afd28a4c82da8092140d641d8de9a6b5eaed3e
Zian Ejaz <zianejaz@gmail.com>
2021-05-17 21:27:00 +0500

    added sort for uk, italy, france, ireland

---
+++ views/components/test.js
@@ -0,0 +1,15 @@
+let a = 6;
+switch(true){
+    case (a<10) :console.log(1)
+    break;
+    case 2:console.log(2)
+    break;
+    case 3:console.log(3)
+    break;
+    case 4:console.log(4)
+    break;
+
+default: console.log('not listed')
+}
+
+
